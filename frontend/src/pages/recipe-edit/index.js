a171f76a30e5e4a2638965fdbf2d46b4dfeb1e18
Mariia Kochneva <mas.vilis092vi@gmail.com>
2026-04-24 18:10:09 +0500

    Fix null safety for recipeIngredients

--- frontend/src/pages/recipe-edit/index.js
+++ frontend/src/pages/recipe-edit/index.js
@@ -30,7 +30,7 @@ const RecipeEdit = ({ onItemDelete }) => {
     measurement_unit: "",
   });
 
-  const [recipeIngredients, setRecipeIngredients] = useState([]);
+  const [recipeIngredients, setRecipeIngredients] = useState(null);
   const [recipeText, setRecipeText] = useState("");
   const [recipeTime, setRecipeTime] = useState(0);
   const [recipeFile, setRecipeFile] = useState(null);
@@ -54,7 +54,10 @@ const RecipeEdit = ({ onItemDelete }) => {
       return setIngredientError("Ингредиент не выбран");
     }
 
-    if (recipeIngredients.find(({ name }) => name === ingredientValue.name)) {
+    if (
+      (recipeIngredients || []).find(
+        ({ name }) => name === ingredientValue.name)
+    ) {
       return setIngredientError("Ингредиент уже выбран");
     }
 
@@ -130,7 +133,7 @@ const RecipeEdit = ({ onItemDelete }) => {
     if (
       recipeText === "" ||
       recipeName === "" ||
-      recipeIngredients.length === 0 ||
+      (recipeIngredients || []).length === 0 ||
       recipeTime === "" ||
       recipeFile === "" ||
       recipeFile === null
@@ -165,7 +168,7 @@ const RecipeEdit = ({ onItemDelete }) => {
             const data = {
               text: recipeText,
               name: recipeName,
-              ingredients: recipeIngredients.map((item) => ({
+              ingredients: (recipeIngredients || []).map((item) => ({
                 id: item.id,
                 amount: item.amount,
               })),
@@ -303,7 +306,7 @@ const RecipeEdit = ({ onItemDelete }) => {
               </p>
             )}
             <div className={styles.ingredientsAdded}>
-              {recipeIngredients.map((item) => {
+              {(recipeIngredients || []).map((item) => {
                 return (
                   <div className={styles.ingredientsAddedItem}>
                     <span className={styles.ingredientsAddedItemTitle}>
